5493fc8e8687b7308e983e87d1a1afd8a425b3fc
kang <wykwork2022@163.com>
2022-05-03 17:01:28 +0800

    second commit

--- src/main/java/com/kang/git/GitTest.java
+++ src/main/java/com/kang/git/GitTest.java
@@ -5,6 +5,8 @@ public class GitTest {
     public static void main(String[] args) {
 
         System.out.println("hello Git! ");
+        // 增加一行
+        System.out.println("hello Git! ");
 
     }
 }
